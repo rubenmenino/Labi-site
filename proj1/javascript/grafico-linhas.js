9d7a428bf220314d4d2d171abc05220fa7d5beb4
renatovalente5 <renato.lima.valente@gmail.com>
2017-12-21 00:23:29 +0000

    melhorei os graficos - "dados estatisticos"

--- proj1/javascript/grafico-linhas.js
+++ proj1/javascript/grafico-linhas.js
@@ -3,6 +3,9 @@ function desenhaGrafico() {
 		title: {
 			text: "Incêndios em Portugal",
 		},
+		exporting: { 
+			enabled: false
+		},
 		xAxis: {
 			categories: ["2001", "2002", "2003", "2004", "2005", "2006",
 				"2007", "2008", "2009", "2010", "2011", "2012", "2003", "2014", "2015", "2016", "2017"]
@@ -17,102 +20,3 @@ function desenhaGrafico() {
 	});
 };
 
-/*
-Highcharts.chart('container', {
-    chart: {
-        type: 'pie',
-        options3d: {
-            enabled: true,
-            alpha: 45,
-            beta: 0
-        }
-    },
-    title: {
-        text: "Causas de Incêndios"
-    },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            depth: 35,
-            dataLabels: {
-                enabled: true,
-                format: '{point.name}'
-            }
-        }
-    },
-    series: [{
-        type: 'pie',
-        name: 'Browser share',
-        data: [
-            ['Negligência', 37],
-            ['Intencional', 19],
-            {
-                name: 'Chrome',
-                y: 12.8,
-                sliced: true,
-                selected: true
-            },
-            ['Natural', 2],
-            ['Desconhecida', 35],
-            ['Reacendimento', 7]
-        ]
-    }]
-});*/
-
-
-Highcharts.chart('container', {
-    chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-    },
-    title: {
-        text: 'Browser market shares January, 2015 to May, 2015'
-    },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                style: {
-                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                }
-            }
-        }
-    },
-    series: [{
-        name: 'Brands',
-        colorByPoint: true,
-        data: [{
-            name: 'IE',
-            y: 56.33
-        }, {
-            name: 'Chrome',
-            y: 24.03,
-            sliced: true,
-            selected: true
-        }, {
-            name: 'Firefox',
-            y: 10.38
-        }, {
-            name: 'Safari',
-            y: 4.77
-        }, {
-            name: 'Opera',
-            y: 0.91
-        }, {
-            name: 'Other',
-            y: 0.2
-        }]
-    }]
-});
